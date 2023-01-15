80c5e293a4fb21e2cf3e9c28e341ad34be74e5f0
Asutosh Variar <asutoshvariar@duck.com>
2023-01-15 22:58:01 +0400

    Tweaks for parity

--- main.py
+++ main.py
@@ -1,30 +1,41 @@
 from PIL import ImageColor
+
+# Open in-folder gtk.css
 f = open('gtk.css', 'r')
 colorDict = {}
 counter = 0
+what = 0
 
-
+# User defined
 name = input('Enter the theme name: ')
 filename = '%s.json' % name
 output = open(filename, 'a')
-output.write('{\n\t"name": "' + name + '",')
-output.write('\n\t"variables": {')
+output.write('{\n    "name": "' + name + '",')
+output.write('\n    "variables": {')
 
+# For loop
 for line in f:
     splitThings = []
+    # Checks to see if line is defining color
     if line[0] == "@":
         splitThings = line.split(" ")
         splitThings[2] = splitThings[2].replace(';','')
         if not splitThings[2][0] == '@':
             colorDict[splitThings[1]] = 'rgb' + str(ImageColor.getcolor(splitThings[2], 'RGB'))
+            output.write('\n        "' + splitThings[1] + ': "' + colorDict[splitThings[1]] + '",')
         else:
-            colorDict[splitThings[1]] = splitThings[2].replace('\n', '')
-        output.write('\n\t\t"' + splitThings[1] + '": "' + colorDict[splitThings[1]] + '",')
+            if splitThings[1] == 'error_color':
+                colorDict[splitThings[1]] = colorDict[splitThings[2].replace('@','').strip()]
+                output.write('\n        "' + splitThings[1] + '": "' + colorDict[splitThings[1]] + '"')
+            else:
+                colorDict[splitThings[1]] = colorDict[splitThings[2].replace('@','').strip()]
+                output.write('\n        "' + splitThings[1] + '": "' + colorDict[splitThings[1]] + '",')
     else:
         if counter == 0:
+            output.write('\n    },')
             output.write('\n')
-            output.write('\t"custom_css": {')
-            output.write('\n\t\t"gtk-4": {"' + '\n')
+            output.write('    "custom_css": {')
+            output.write('\n        "gtk-4": "')
 
         line.replace('\n','')
 
@@ -32,7 +43,7 @@ for line in f:
         counter += 1
 
 output.write('"'.rstrip('\n'))
-output.write('\n\t}')
+output.write('\n    }')
 output.write('\n}')
 
 f.close()
